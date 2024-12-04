ad76981844ab6249a2ee2f656bcfbf8c8e22229f
Iftakhir Mozumder Nibir <134167711+IftakhirNibir@users.noreply.github.com>
2024-12-05 05:25:56 +0600

    add javascript file

---
+++ scripts.js
@@ -0,0 +1,28 @@
+let recognition;
+let isRecognizing = false;
+
+//hide icon initially
+$('#playIcon').hide()
+
+//checking is support in browser
+if ('webkitSpeechRecognition' in window || 'SpeechRecognition' in window) {
+  const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+  recognition = new SpeechRecognition();
+  recognition.continuous = true;
+  recognition.lang = 'bn-BD';
+
+  recognition.onresult = function(event) {
+    let transcript = '';
+    for (let i = event.resultIndex; i < event.results.length; i++) {
+      transcript += event.results[i][0].transcript + " ";
+    }
+    $('#output-text').val($('#output-text').val() + transcript);
+  };
+
+//auto restart
+  recognition.onend = function () {
+    if (isRecognizing) {
+      recognition.start();
+    }
+  };
+}
